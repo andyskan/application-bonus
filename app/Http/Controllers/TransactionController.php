7abb0f8666eaafa18da6f6d49358dbb137d83954
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2019-10-29 16:59:30 +0800

    refactored function and added comments

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -18,11 +18,23 @@ class TransactionController extends Controller
     {
         //
     }
+    /**
+     * Retreive all transaction history
+     *
+     * @return mixed
+     */
     public function getAllTransaction()
     {
         $data = Transaction::all();
         return response($data);
     }
+    /**
+     * Create a new transaction
+     *
+     * @param  mixed $request
+     *
+     * @return void
+     */
     public function newTransaction(Request $request)
     {
         $transactionList = Transaction::where('user_id', $request->input('user_id'))
@@ -31,20 +43,12 @@ class TransactionController extends Controller
         if (count($transactionList) > 0) {
             return response("User has received daily rewards");
         } else {
-            $user = BonusUser::find($request->input('user_id'));
-            $randomNumber = mt_rand($user->low_range, $user->top_range);
-            $rewardAmount = ceil($randomNumber / 5000) * 5000;
 
-            $dailyRewards = DailyRewards::where('id', $request->input('daily_reward_id'))
-                ->lockForUpdate()->first();
-            if ($dailyRewards->current_value - $rewardAmount <= 0) {
-                $rewardAmount = $dailyRewards->current_value;
-                $dailyRewards->current_value = 0;
-
-            } else {
-                $dailyRewards->current_value -= $rewardAmount;
-            }
-            $dailyRewards->save();
+            $randomReward = $this->getRewardAmount($request->input('user_id'));
+            $rewardAmount = $this->getDailyRewardAmount(
+                $randomReward,
+                $request->input('daily_reward_id')
+            );
 
             $transaction = new Transaction();
             $transaction->user_id = $request->input('user_id');
@@ -54,6 +58,45 @@ class TransactionController extends Controller
 
             return response($transaction);
         }
+    }
+    /**
+     * Get daily reward amount, calculates whether the reward amount retrieved is beyond limit
+     *
+     * @param  integer $rewardAmount
+     * @param  integer $dailyRewardsId
+     *
+     * @return void
+     */
+    public function getDailyRewardAmount($rewardAmount, $dailyRewardsId)
+    {
+        $dailyRewards = DailyRewards::where('id', $dailyRewardsId)
+            ->lockForUpdate()->first();
+        if ($dailyRewards->current_value - $rewardAmount <= 0) {
+            $rewardAmount = $dailyRewards->current_value;
+            $dailyRewards->current_value = 0;
+
+        } else {
+            $dailyRewards->current_value -= $rewardAmount;
+        }
+        $dailyRewards->save();
+        return $rewardAmount;
+
+    }
+
+    /**
+     * Get random reward amount
+     *
+     *
+     * @param  mixed $userId
+     *
+     * @return void
+     */
+    public function getRewardAmount($userId)
+    {
+        $user = BonusUser::find($userId);
+        $randomNumber = mt_rand($user->low_range, $user->top_range);
+        $rewardAmount = ceil($randomNumber / 5000) * 5000;
+        return $rewardAmount;
     }
 
     //
